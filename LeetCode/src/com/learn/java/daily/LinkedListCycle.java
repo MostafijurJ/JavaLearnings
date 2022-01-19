a6cbe856889133595695fdba18eb840e91a03172
mostafijurr <mostafijur.rahman@brainstation23.com>
2022-01-20 00:18:50 +0600

    LeetCode: Linked List Cycle easy and medium solution

--- LeetCode/src/com/learn/java/daily/LinkedListCycle.java
+++ LeetCode/src/com/learn/java/daily/LinkedListCycle.java
@@ -1,7 +1,57 @@
 package com.learn.java.daily;
 
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
+
 public class LinkedListCycle {
   public static void main(String[] args) {
 
+    System.out.println(detectCycle(null));
+  }
+
+  private static ListNode detectCycle(ListNode head) {
+    if (head == null || head.next == null) return null;
+    ListNode slow = head, fast = head;
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        fast = head;
+        while (slow != fast) {
+          slow = slow.next;
+          fast = fast.next;
+        }
+        return slow;
+      }
+    }
+    return null;
+  }
+
+
+  /**
+   * Easy version of detect cycle
+   *
+   * @param head
+   * @return
+   */
+
+  public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) return false;
+    ListNode slow = head, fast = head;
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
   }
 }
