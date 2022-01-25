20358b9827f4b28992ee3000e049c6d5be345b02
mostafijurr <mostafijur.rahman@brainstation23.com>
2022-01-25 23:14:18 +0600

    LeetCode: main class segregated

--- LeetCode/src/com/learn/java/daily/DetectCapital.java
+++ LeetCode/src/com/learn/java/daily/DetectCapital.java
@@ -1,11 +1,7 @@
 package com.learn.java.daily;
 
 public class DetectCapital {
-  public static void main(String[] args) {
-    System.out.println(new DetectCapital().detectCapital("aaaaa"));
-  }
-
-  private boolean detectCapital(String word) {
+  public boolean detectCapital(String word) {
     int count = 0;
     for (int i = 0; i < word.length(); i++)
       if (word.charAt(i) - 'A' < 26) count++;
---
+++ LeetCode/src/com/learn/java/daily/MainClass.java
@@ -0,0 +1,11 @@
+package com.learn.java.daily;
+
+public class MainClass {
+  public static void main(String[] args) {
+
+    /**
+     * Detect capital
+     */
+    System.out.println(new DetectCapital().detectCapital("USA"));
+  }
+}
