15486e48d998992bc00cc81fd09387cdb2c183c2
Mostafijur Rahman <mrkajol550@gmail.com>
2022-01-29 03:15:51 +0600

    LeetCode: finding maximum XOR

---
+++ LeetCode/src/com/learn/java/daily/MaximumXOR.java
@@ -0,0 +1,22 @@
+package com.learn.java.daily;
+
+public class MaximumXOR {
+  public static void main(String[] args) {
+    System.out.println(findMaximumXOR(new int[] {3,10,5,25,2,8})); //
+    System.out.println(findMaximumXOR(new int[] {14,70,53,83,49,91,36,80,92,51,66,70})); //
+  }
+
+    public static int findMaximumXOR(int[] nums) {
+       int maxXOR = 0;
+       for (int i = 0; i < nums.length; i++) {
+         for (int j = i + 1; j < nums.length; j++) {
+           maxXOR = Math.max(maxXOR, xor(nums[i], nums[j]));
+         }
+       }
+       return maxXOR;
+    }
+
+    static int xor(int a, int b) {
+      return a ^ b;
+    }
+}
---
+++ LeetCode/src/com/learn/java/xor/XOR.java
@@ -0,0 +1,14 @@
+package com.learn.java.xor;
+
+public class XOR {
+  public static void main(String[] args) {
+
+  }
+
+
+
+  static int xor(int a, int b) {
+    return a ^ b;
+  }
+
+}
